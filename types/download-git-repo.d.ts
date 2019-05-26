ba0cc1036e410c541ba28ddac6ef26913a643220
Chersquwn <Chersquwn@163.com>
2019-05-26 23:00:49 +0800

    feat: add types

---
+++ types/download-git-repo.d.ts
@@ -0,0 +1,2 @@
+export = index
+declare function index(repo: any, dest: any, opts: any, fn: any): void
---
+++ types/validate-npm-package-name.d.ts
@@ -0,0 +1,6 @@
+export = index
+declare function index(name: any): any
+// eslint-disable-next-line no-redeclare
+declare namespace index {
+  const scopedPackagePattern: RegExp
+}
